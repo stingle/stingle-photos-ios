7fa1b0a82f72bb32757aa77a5e3d6d309282937f
Khoren Asatryan <khorenasatryan13@gmail.com>
2021-03-17 01:03:30 +0400

    tmp commit

--- Stingle/New/Common/Extensions/Date+Operation.swift
+++ Stingle/New/Common/Extensions/Date+Operation.swift
@@ -2,14 +2,14 @@ import Foundation
 
 extension Date {
     
+    static var defaultDate: Date {
+        return Date(milliseconds: 0)
+    }
+    
     var millisecondsSince1970: UInt64 {
         return UInt64((self.timeIntervalSince1970 * 1000.0).rounded())
     }
     
-    static var defaultDate: Date {
-        return Date(milliseconds: 0)
-    }
-
     init(milliseconds: UInt64) {
         self = Date(timeIntervalSince1970: TimeInterval(milliseconds) / 1000)
     }
