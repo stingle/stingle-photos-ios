38bbefa459d211838bb5f4aac9e92d03347832cd
Khoren Asatryan <khorenasatryan13@gmail.com>
2021-08-10 03:19:23 +0400

    tmp commit

--- Stingle/New/Scense/Settings/BackupPhrase/STBackupPhraseVC.swift
+++ Stingle/New/Scense/Settings/BackupPhrase/STBackupPhraseVC.swift
@@ -8,13 +8,38 @@
 import UIKit
 
 class STBackupPhraseVC: UIViewController {
-
+    
+    @IBOutlet weak private var titleLabel: UILabel!
+    @IBOutlet weak private var descriptionLabel: UILabel!
+    @IBOutlet weak private var backupPhraseButton: STButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.configureLocalized()
     }
     
-
+    //MARK: - Private methoth
+    
+    private func configureLocalized() {
+        self.navigationItem.title = "menu_backup_phrase".localized
+        self.titleLabel.text = "menu_backup_phrase".localized
+        self.descriptionLabel.text = "backup_phrase_description".localized
+        self.backupPhraseButton.setTitle("ok_get_backup_phrase".localized, for: .normal)
+    }
+    
+    //MARK: - User action
+    
+    @IBAction private func didSelectBackupPhraseButton(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction private func didSelectMenuBarItem(_ sender: Any) {
+        if self.splitMenuViewController?.isMasterViewOpened ?? false {
+            self.splitMenuViewController?.hide(master: true)
+        } else {
+            self.splitMenuViewController?.show(master: true)
+        }
+    }
 
 }
